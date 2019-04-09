a825345540d8f179c28924ab11091fdeb4c73ace
Mac Heller-Ogden <mac@machellerogden.com>
2019-04-09 16:18:32 -0500

    adding repl support

--- src/edn_to_js/main.cljs
+++ src/edn_to_js/main.cljs
@@ -2,6 +2,11 @@
   (:require [cljs.nodejs :as nodejs]
             [cljs.reader :as r]))
 
-(defn ^:export edn->js [edn]
+(defn edn->js [edn]
   (let [data (r/read-string edn)]
     (clj->js data)))
+
+(defn reload! []
+  (println "Code updated."))
+
+(defn main! [])
